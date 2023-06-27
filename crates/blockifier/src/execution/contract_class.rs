57d1ed51bca73d77b7383dd79b9e6b6fa723de70
greged93 <82421016+greged93@users.noreply.github.com>
2023-06-27 16:34:12 +0200

    feat: add v1 contract class scale encoding decoding (#6)
    * add v1 contract class scale encoding decoding
    
    * fix rebasing
    
    * update comments github

--- crates/blockifier/src/execution/contract_class.rs
+++ crates/blockifier/src/execution/contract_class.rs
@@ -9,6 +9,7 @@ use cairo_vm::types::program::Program;
 use cairo_vm::types::relocatable::MaybeRelocatable;
 use cairo_vm::vm::runners::builtin_runner::{HASH_BUILTIN_NAME, POSEIDON_BUILTIN_NAME};
 use cairo_vm::vm::runners::cairo_runner::ExecutionResources as VmExecutionResources;
+#[cfg(feature = "parity-scale-codec")]
 use parity_scale_codec::{Decode, Encode};
 use serde::de::Error as DeserializationError;
 use serde::{Deserialize, Deserializer, Serialize};
@@ -32,6 +33,7 @@ use crate::stdlib::vec::Vec;
 // Note: when deserializing from a SN API class JSON string, the ABI field is ignored
 // by serde, since it is not required for execution.
 #[derive(Clone, Debug, Eq, PartialEq, derive_more::From, Deserialize)]
+#[cfg_attr(feature = "parity-scale-codec", derive(Encode, Decode))]
 pub enum ContractClass {
     V0(ContractClassV0),
     V1(ContractClassV1),
@@ -54,7 +56,8 @@ impl ContractClass {
 }
 
 // V0.
-#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Encode, Decode)]
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq)]
+#[cfg_attr(feature = "parity-scale-codec", derive(Encode, Decode))]
 pub struct ContractClassV0(pub Arc<ContractClassV0Inner>);
 impl Deref for ContractClassV0 {
     type Target = ContractClassV0Inner;
@@ -86,7 +89,7 @@ impl ContractClassV0 {
             + self.n_builtins()
             + self.bytecode_length()
             + 1; // Hinted class hash.
-        // The hashed data size is approximately the number of hashes (invoked in hash chains).
+                 // The hashed data size is approximately the number of hashes (invoked in hash chains).
         let n_steps = constants::N_STEPS_PER_PEDERSEN * hashed_data_size;
 
         VmExecutionResources {
@@ -112,6 +115,7 @@ pub struct ContractClassV0Inner {
     pub entry_points_by_type: HashMap<EntryPointType, Vec<EntryPoint>>,
 }
 
+#[cfg(feature = "parity-scale-codec")]
 impl Encode for ContractClassV0Inner {
     fn encode(&self) -> Vec<u8> {
         let val = self.clone();
@@ -123,6 +127,7 @@ impl Encode for ContractClassV0Inner {
     }
 }
 
+#[cfg(feature = "parity-scale-codec")]
 impl Decode for ContractClassV0Inner {
     fn decode<I: parity_scale_codec::Input>(
         input: &mut I,
@@ -147,6 +152,7 @@ impl TryFrom<DeprecatedContractClass> for ContractClassV0 {
 
 // V1.
 #[derive(Clone, Debug, Default, Eq, PartialEq, Deserialize)]
+#[cfg_attr(feature = "parity-scale-codec", derive(Encode, Decode))]
 pub struct ContractClassV1(pub Arc<ContractClassV1Inner>);
 impl Deref for ContractClassV1 {
     type Target = ContractClassV1Inner;
@@ -219,7 +225,37 @@ pub struct ContractClassV1Inner {
     pub hints: HashMap<String, Hint>,
 }
 
+#[cfg(feature = "parity-scale-codec")]
+impl Encode for ContractClassV1Inner {
+    fn encode(&self) -> Vec<u8> {
+        let val = self.clone();
+        let entry_points_by_type = val
+            .entry_points_by_type
+            .into_iter()
+            .collect::<Vec<(EntryPointType, Vec<EntryPointV1>)>>();
+        let hints = val.hints.into_iter().collect::<Vec<(String, Hint)>>();
+        (val.program, entry_points_by_type, hints).encode()
+    }
+}
+
+#[cfg(feature = "parity-scale-codec")]
+impl Decode for ContractClassV1Inner {
+    fn decode<I: parity_scale_codec::Input>(
+        input: &mut I,
+    ) -> Result<Self, parity_scale_codec::Error> {
+        let res =
+            <(Program, Vec<(EntryPointType, Vec<EntryPointV1>)>, Vec<(String, Hint)>)>::decode(
+                input,
+            )?;
+        let entry_points_by_type =
+            <HashMap<EntryPointType, Vec<EntryPointV1>>>::from_iter(res.1.into_iter());
+        let hints = <HashMap<String, Hint>>::from_iter(res.2.into_iter());
+        Ok(ContractClassV1Inner { program: res.0, entry_points_by_type, hints })
+    }
+}
+
 #[derive(Debug, Default, Clone, Eq, PartialEq, Hash, Serialize, Deserialize)]
+#[cfg_attr(feature = "parity-scale-codec", derive(Encode, Decode))]
 pub struct EntryPointV1 {
     pub selector: EntryPointSelector,
     pub offset: EntryPointOffset,
@@ -349,15 +385,22 @@ fn convert_entry_points_v1(
 }
 
 #[cfg(test)]
+#[cfg(feature = "parity-scale-codec")]
 mod test {
     use parity_scale_codec::{Decode, Encode};
 
-    use crate::execution::contract_class::ContractClassV0;
-    use crate::test_utils::TEST_CONTRACT_PATH;
+    use crate::execution::contract_class::{ContractClassV0, ContractClassV1};
+    use crate::test_utils::{TEST_CONTRACT_CAIRO1_PATH, TEST_CONTRACT_PATH};
 
     #[test]
     fn test_encode_decode_contract_v0() {
         let contract = ContractClassV0::from_file(TEST_CONTRACT_PATH);
         assert_eq!(contract, ContractClassV0::decode(&mut &contract.encode()[..]).unwrap())
+    }
+
+    #[test]
+    fn test_encode_decode_contract_v1() {
+        let contract = ContractClassV1::from_file(TEST_CONTRACT_CAIRO1_PATH);
+        assert_eq!(contract, ContractClassV1::decode(&mut &contract.encode()[..]).unwrap())
     }
 }
