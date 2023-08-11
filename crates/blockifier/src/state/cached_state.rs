b8cac040aa4cc7ae10fcbfe2651d0042d56601db
apoorvsadana <95699312+apoorvsadana@users.noreply.github.com>
2023-08-11 12:57:04 +0530

    fix: no_std with testing feature

--- crates/blockifier/src/state/cached_state.rs
+++ crates/blockifier/src/state/cached_state.rs
@@ -331,7 +331,7 @@ impl<S: StateReader> State for CachedState<S> {
     }
 }
 
-#[cfg(any(feature = "testing", test))]
+#[cfg(any(test))]
 impl Default for CachedState<crate::test_utils::DictStateReader> {
     fn default() -> Self {
         Self {
