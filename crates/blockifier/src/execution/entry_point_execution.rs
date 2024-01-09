c2250057c0602449f6ed317419543a5019dd1e81
Ben Goebel <dontpanicdao@gmail.com>
2024-01-09 14:59:49 -0700

    callres clone

--- crates/blockifier/src/execution/entry_point_execution.rs
+++ crates/blockifier/src/execution/entry_point_execution.rs
@@ -38,8 +38,7 @@ pub struct VmExecutionContext<'a> {
     pub program_extra_data_length: usize,
 }
 
-#[cfg_attr(feature = "clone", derive(Clone))]
-#[derive(Debug, Default, Eq, PartialEq)]
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct CallResult {
     pub failed: bool,
     pub retdata: Retdata,
